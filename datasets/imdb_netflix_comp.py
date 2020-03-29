6ae4aae3ac17291ce48c3383f573e64f097d034c
HENRIC <vincent.henric@hotmail.fr>
2020-03-29 15:12:19 +0200

    add script for imdb comp

---
+++ datasets/imdb_netflix_comp.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 10 21:54:36 2020
+
+@author: henric
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from pyspark.sql import *
+import pyspark.sql.functions as F
+
+spark = SparkSession \
+    .builder \
+    .appName("Privacy Project") \
+    .getOrCreate()
+    
+netflix = spark.read.csv("ratings.csv", inferSchema=True, header=True)
+imdb    = spark.read.csv("datasets/imdb_ratings.csv", inferSchema=True, header=True, sep=",")
+
+netflix = netflix.drop('custId', 'date').groupBy('movieId').agg(F.mean('rating').alias('averageRating_netflix'),
+                                                      F.count('rating').alias('numVotes_netflix'))
+
+netflix.join(imdb.drop('_c0'), 'movieId').toPandas().to_csv('common_movies.csv', index=False)
